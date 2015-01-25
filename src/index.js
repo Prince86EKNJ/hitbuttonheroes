4ab5c9632607d86c6806aa80e4a9354e0074d228
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2015-01-25 17:46:53 -0600

    Added "modelines" to build script and refactored "index.js"

--- src/index.js
+++ src/index.js
@@ -8,19 +8,21 @@ var widget = require("./widget");
 
 var fps = 30;
 
+var init = function(views)
+{
+	var install = widget(views);
+
+	var data = { dataValue: "Your Mom is da bomb!", time: 0 };
+	install("body", "time-demo", data);
+
+	_.watch(Date.now, 1000/fps, function(time)
+	{
+		$(".time").text(data.dataValue+":"+time);
+		data.time = time;
+	});
+};
+
 $(function()
 {
-	$.get("/data/views.json", function(views)
-	{
-		var install = widget(views);
-
-		var data = { dataValue: "Your Mom is da bomb!", time: 0 };
-		install("body", "time-demo", data);
-
-		_.watch(Date.now, 1000/fps, function(time)
-		{
-			$(".time").text(data.dataValue+":"+time);
-			data.time = time;
-		});
-	});
+	$.get("/data/views.json", init);
 });
