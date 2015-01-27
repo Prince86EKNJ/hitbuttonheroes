a88bbaff46506b66e112c61197d8af3861c3ca5b
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2015-01-27 00:34:34 -0600

    Draft ideas for "well" and "keyman"

--- src/index.js
+++ src/index.js
@@ -27,6 +27,39 @@ var init = function(views)
 	{
 		data.time = time;
 		meterData.progress = (time % 5000) / 5000;
+
+/*
+		var duration = time - lastTime;
+		lastTime = time;
+		keyMan.run(duration);
+*/
+	});
+
+	var getRandomMsg = function()
+	{
+		return "The number is: "+Math.random()*100 % 1;
+	};
+
+	// Setup well for meter
+	var well = new Well(75);
+	var rate = 30;
+	keyMan.onDown("z", function()
+	{
+		meterData.msg = getRandomMsg();
+	});
+	keyMan.onUp("z", function()
+	{
+		meterData.msg = getRandomMsg();
+	});
+	keyMan.whileDown("x", function(duration)
+	{
+		var fill = (duration/1000) * rate;
+		well.add(fill);
+	});
+	keyMan.whileUp("x", function(duration)
+	{
+		var drain = (duration/1000) * rate;
+		well.add(drain);
 	});
 };
 
